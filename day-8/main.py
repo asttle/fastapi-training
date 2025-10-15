c239100664cfdd459044a6e376536ae6bda0ee49
asttle-sigtech <asttle1997@gmail.com>
2025-10-15 21:39:58 +0100

    chore: fastapi jwt intermediate commit

---
+++ day-8/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, HTTPException, Depends
+from typing import Annotated
+from database import SessionLocal, engine
+import models
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/", status_code=200)
+async def user(user: None, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    return {"User": user}
+
